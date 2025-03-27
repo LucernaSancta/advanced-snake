a69afd7ff59ca521ba80e99ee2facf2f40c98e38
LucernaSancta <tommaso.pattaro@hotmail.com>
2025-03-27 18:26:17 +0100

    Head texture support

--- game_objects.py
+++ game_objects.py
@@ -1,16 +1,15 @@
+import pygame
 from pygame.math import Vector2
-from pygame.color import Color
-from pygame.surface import Surface
-from pygame import draw
+
 from various import key_map
 
 class Snake:
     def __init__(self,
-                 color: Color,
+                 color: pygame.color.Color,
                  keybindings: key_map,
                  pos: Vector2 = Vector2(0,0),
                  length: int = 3,
-                 textures: str = '',
+                 textures: str = 'textures\\snakes\\testing.png',
                  direction: Vector2 = Vector2(0,0),
                  thikness: Vector2 = Vector2(10,10)
                  ) -> None:
@@ -18,9 +17,12 @@ class Snake:
         self.color = color
         self.keybindings = keybindings
         self.pos = pos
-        self.textures = textures
         self.direction = direction
         self.thikness = thikness
+
+        # Load the textures
+        self.textures = pygame.image.load(textures).convert()
+        self.textures = pygame.transform.scale(self.textures, (thikness.x*3, thikness.y*6))
 
         # State 0 is 'not moving', 1 is normal and 2 is dead
         self.state = 0
@@ -35,8 +37,8 @@ class Snake:
 
         match key:
             case self.keybindings.up:    self.direction = Vector2(0,-1)
-            case self.keybindings.down:  self.direction = Vector2(0,1)
             case self.keybindings.left:  self.direction = Vector2(-1,0)
+            case self.keybindings.down:  self.direction = Vector2(0,1)
             case self.keybindings.right: self.direction = Vector2(1,0)
     
     def update(self) -> None:
@@ -58,13 +60,19 @@ class Snake:
         for _ in range(power):
             self.pieces.append(self.last_removed)
     
-    def frame(self, display: Surface) -> None:
+    def frame(self, display: pygame.surface.Surface) -> None:
 
         # 0 is not moving and 1 is normal
         if self.state in [0,1]:
-            draw.rect(display, self.color, (self.pos, self.thikness))
+
+            # Head
+            if   self.direction == Vector2(0,-1): display.blit(self.textures, self.pos, (0, self.thikness.y*0, self.thikness.x, self.thikness.y)); print('hi1')
+            elif self.direction == Vector2(-1,0): display.blit(self.textures, self.pos, (0, self.thikness.y*1, self.thikness.x, self.thikness.y)); print('hi2')
+            elif self.direction == Vector2(0,1):  display.blit(self.textures, self.pos, (0, self.thikness.y*2, self.thikness.x, self.thikness.y)); print('hi3')
+            elif self.direction == Vector2(1,0):  display.blit(self.textures, self.pos, (0, self.thikness.y*3, self.thikness.x, self.thikness.y)); print('hi4')
+
             for piece in self.pieces:
-                draw.rect(display, self.color, (piece, self.thikness))
+                pygame.draw.rect(display, self.color, (piece, self.thikness))
         # Death state
         elif self.state == 2:
             ...
@@ -73,7 +81,7 @@ class Snake:
         raise NotImplementedError
 
 class Walls:
-    def __init__(self, external_box: Vector2, custom_walls: list[Vector2], thikness: Vector2, color: Color):
+    def __init__(self, external_box: Vector2, custom_walls: list[Vector2], thikness: Vector2, color: pygame.color.Color):
         self.external_box = external_box
         self.custom = custom_walls
         self.color = color
@@ -89,18 +97,18 @@ class Walls:
         
         return False
     
-    def frame(self, display: Surface):
-        # Draw the custom walls
+    def frame(self, display: pygame.surface.Surface):
+        # pygame.draw the custom walls
         for wall in self.custom:
-            draw.rect(display, self.color, (wall, self.thikness))
+            pygame.draw.rect(display, self.color, (wall, self.thikness))
 
 class Apple:
-    def __init__(self, pos: Vector2, power: int, thikness: Vector2, color: Color, texture: str = ''):
+    def __init__(self, pos: Vector2, power: int, thikness: Vector2, color: pygame.color.Color, texture: str = ''):
         self.pos = pos
         self.power = power
         self.thikness = thikness
         self.color = color
         self.texture = texture
     
-    def frame(self, display: Surface):
-        draw.rect(display, self.color, (self.pos, self.thikness))
+    def frame(self, display: pygame.surface.Surface):
+        pygame.draw.rect(display, self.color, (self.pos, self.thikness))
