cf4fa43114a94fcf7e313f22670eebddeb072693
Lucernasancta <tommaso.pattaro@hotmail.com>
2025-06-01 01:17:49 +0200

    Cleaned Bad_apple code

--- foods/Bad_apple.py
+++ foods/Bad_apple.py
@@ -1,12 +1,24 @@
-from .default import Food
+import pygame
+from pygame.math import Vector2
+
+from logger import logger as log
 from game_objects import Snake
+from .default import Food
+
 
 class Bad_apple(Food):
+
     def initialize(self) -> None:
         self.damage = self.kwargs['damage']
         self.init_texture('bad_apple.png')
+    
+    def eaten(
+            self,
+            surface: pygame.surface.Surface,
+            snake: Snake,
+            snakes: list[Snake]
+        ) -> None:
 
-    def eaten(self, surface, snake: Snake, snakes: list[Snake]) -> None:
         for _ in range(self.damage):
             if len(snake.pieces) > 1:
                 snake.pieces.pop()
